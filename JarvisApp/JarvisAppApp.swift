3d84d3877e0d69a35c0ca897b9727cd50ba02be8
min-taeho <taehodot@gmail.com>
2026-05-22 17:52:45 +0900

    Initial Commit

---
+++ JarvisApp/JarvisAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JarvisAppApp.swift
+//  JarvisApp
+//
+//  Created by 민태호 on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct JarvisAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JarvisAppTests/JarvisAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  JarvisAppTests.swift
+//  JarvisAppTests
+//
+//  Created by 민태호 on 5/22/26.
+//
+
+import Testing
+@testable import JarvisApp
+
+struct JarvisAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
